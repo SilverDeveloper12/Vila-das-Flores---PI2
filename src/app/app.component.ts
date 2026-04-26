ffad68b4ecdce9385f9e24a80fac0b42889f2824
SilverDeveloper12 <develop.silversol@gmail.com>
2026-04-26 14:57:05 -0300

    Organização para SPA
    organização dos arquivos, remoção de excesso, alteração de locais para SPA e inicio da implementação de troca de paginas dinâmicas no angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    UsuariosComponent,
-    ProdutosComponent
+    RouterModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
---
+++ src/app/pages/contatos/contatos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos.component',
+  imports: [],
+  templateUrl: './contatos.component.html',
+  styleUrl: './contatos.component.css',
+})
+export class ContatosComponent {}
