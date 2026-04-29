f985006b1c4cfcb13bb92f9477d9bb0ccbb24b65
Barbiripytanga <ollibarbara1@gmail.com>
2026-04-29 00:26:31 -0300

    Tratativa de possíveis erros do usuário
    -Validação do e-mail;
    -Senha mínima obrigatória;
    -Add campo confirmar senha;
    -Validação do telefone;
    -Bloqueio de e-mail duplicado;
    -Feedbacks ao usuário.

--- src/app/pages/cadastro-cliente/cadastro-cliente.ts
+++ src/app/pages/cadastro-cliente/cadastro-cliente.ts
@@ -12,8 +12,9 @@ import { Usuario } from '../../models/models';
   styleUrl: './cadastro-cliente.css'
 })
 export class CadastroClienteComponent {
-
-  novoUsuario: Usuario = {
+confirmarSenha: string = '';
+ 
+novoUsuario: Usuario = {
     tipo: 'cliente',
     nome: '',
     email: '',
@@ -22,12 +23,52 @@ export class CadastroClienteComponent {
     endereco: ''
   };
 
+
   constructor(
     private apiService: ApiService,
     private router: Router
   ) {}
 
-  cadastrar() {
+ cadastrar() {
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(this.novoUsuario.email)) {
+    alert('Digite um e-mail válido.');
+    return;
+  }
+
+  if (this.novoUsuario.senha.length < 8) {
+    alert('A senha deve ter no mínimo 8 caracteres.');
+    return;
+  }
+
+  if (this.novoUsuario.senha !== this.confirmarSenha) {
+    alert('As senhas não coincidem.');
+    return;
+  }
+
+  if (!/^\d+$/.test(this.novoUsuario.telefone)) {
+    alert('Telefone deve conter apenas números.');
+    return;
+  }
+
+  if (this.novoUsuario.telefone.length > 11) {
+    alert('Telefone deve ter no máximo 11 números.');
+    return;
+  }
+
+  this.apiService.getUsuarios().subscribe((usuarios) => {
+
+    const emailExiste = usuarios.some(
+      (u: any) => u.email.toLowerCase() === this.novoUsuario.email.toLowerCase()
+    );
+
+    if (emailExiste) {
+      alert('Este e-mail já está cadastrado.');
+      return;
+    }
+
     this.apiService.createUsuario(this.novoUsuario).subscribe({
       next: () => {
         alert('Cadastro realizado com sucesso!');
@@ -37,5 +78,8 @@ export class CadastroClienteComponent {
         alert('Erro ao cadastrar usuário.');
       }
     });
-  }
+
+  });
+
+ }
 }
